598f54eb772abdb5f0f068f354a5baaa8161875c
Claudia Kiesenhofer <c.kiesenhofer@gmail.com>
2016-04-16 11:40:40 -0600

    Pass test admin login sad path

--- test/integration/admin_login_test.rb
+++ test/integration/admin_login_test.rb
@@ -7,10 +7,24 @@ class AdminLoginTest < ActionDispatch::IntegrationTest
                         role: 1)
 
     ApplicationController.any_instance.stubs(:current_user).returns(admin)
-    
+
     visit admin_categories_path
 
     assert page.has_content?("All Categories")
+  end
+
+  test "default user cannot login and view the categories index page" do
+    user = User.create(username: "user",
+                       password: "password",
+                       role: 0)
+
+    ApplicationController.any_instance.stubs(:current_user).returns(user)
+
+    visit admin_categories_path
+
+    refute page.has_content?("All Categories")
+    # assert_equal "/public/404", current_path
+    assert page.has_content?("The page you were looking for doesn't exist (404)")
   end
 
 end
